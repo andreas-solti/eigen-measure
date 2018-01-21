ad35dad1d0ec114b0bfcaecb7009fee2ef601c5b
andreas-solti <andreas.solti@gmail.com>
2018-01-21 15:48:24 +0100

    Simple test

---
+++ src/test/java/de/solti/eigenvalue/test/TestEigenvalue.java
@@ -0,0 +1,11 @@
+package de.solti.eigenvalue.test;
+
+import org.junit.Test;
+
+public class TestEigenvalue {
+
+    @Test
+    public void test1() {
+        System.out.println("Test 1 works");
+    }
+}
